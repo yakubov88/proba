2e79d21330af9c438bcedd79e34c40f5cc34d3f3
yakubov88 <y.yakuboov@gmail.com>
2019-05-17 13:23:18 +0300

    autosearch
    autosearch

--- javascriptvalidateinput.js
+++ javascriptvalidateinput.js
@@ -8,3 +8,17 @@ function Validate(event) {
 }
 
 <input ID="txtcheck" onkeypress="return Validate(event);" />
+    
+    
+    
+    $("#part_qty").keyup(function () {
+        var filter = $(this).val();
+        console.log(filter);
+        $(".part_qty").each(function () {
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).parent().fadeOut();
+            } else {
+                $(this).parent().show();
+            }
+        });
+    });
